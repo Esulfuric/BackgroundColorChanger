b3844d145e66256bcfc825880fe1654b4c846e57
Desmond Bassey <87496340+Esulfuric@users.noreply.github.com>
2021-07-23 20:33:34 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var btn_red;
+var btn_green;
+
+var r1 = 0;
+var g1 = 0;
+var b1 = 0;
+
+
+function setup() {
+  createCanvas(400, 400);  
+  var red = createSprite(100, 200, 50, 100)
+  
+
+  var green = createSprite(300, 200, 50, 100)
+  
+}
+
+function draw() {
+  background(r1,g1,b1);
+  drawSprites()
+}
+
